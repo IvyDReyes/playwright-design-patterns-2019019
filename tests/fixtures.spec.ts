4bd8df584b75df5481f06aabe8edd78897363a0a
IVY D <71013378+IvyDReyes@users.noreply.github.com>
2025-06-17 15:46:10 +1200

    sample commit

--- tests/fixtures.spec.ts
+++ tests/fixtures.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("Sign In button is visible with Fixtures", async ({ page }) => {
+test("Sign In button is visible with lFixtures", async ({ page }) => {
 	await page.goto("https://binaryville.com/account");
 	const signInButton = page.getByRole("button", { name: "Sign in" });
 	await expect(signInButton).toBeVisible();
